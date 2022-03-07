88874e8245e89c15932949435b8f4972962c3f4a
Angel <98554114+ziyuangelli@users.noreply.github.com>
2022-03-07 14:41:21 -0500

    Add files via upload

--- startup_namer/ios/Flutter/flutter_export_environment.sh
+++ startup_namer/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=E:\flutter\flutter_windows_2.10.2-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Engineering\project\myapp"
+export "FLUTTER_APPLICATION_PATH=D:\Engineering\project\myapp\startup_namer"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
